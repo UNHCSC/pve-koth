68ef286c98e357576b2416de132bdc39b7a8c57c
z46-dev <egp1042@usnh.edu>
2025-12-10 15:33:47 -0500

    Fixed tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 export const content = [
-    "./public/views/**/*.html",
-    "./public/static/src/js/**/*.js"
+    "./public/views/*.html",
+    "./public/static/**/*.js"
 ];
-
-export const theme = {
-    extend: {}
-};
-
-export const plugins = [];
